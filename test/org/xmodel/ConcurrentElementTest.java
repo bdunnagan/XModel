ce2e6ca42bee8d3074a168e6c4ffb82d1198ae4e
bdunnagan <bdunnagan@nc.rr.com>
2012-06-09 13:35:15 -0400

    ...

---
+++ test/org/xmodel/ConcurrentElementTest.java
@@ -0,0 +1,132 @@
+package org.xmodel;
+
+import static org.junit.Assert.assertTrue;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+
+/**
+ * Test concurrent read/write access to elements.
+ */
+public class ConcurrentElementTest
+{
+  @Test public void oneWriterOneReader() throws Exception
+  {
+    runTest( 1, 1);
+  }
+  
+  @Test public void oneWriteManyReaders() throws Exception
+  {
+    runTest( 1, 100);
+  }
+  
+  @Test public void manyWritersOneReader() throws Exception
+  {
+    runTest( 100, 1);
+  }
+  
+  @Test public void manyWritersManyReaders() throws Exception
+  {
+    runTest( 100, 100);
+  }
+  
+  private void runTest( int readers, int writers) throws Exception
+  {
+    ExecutorService readerPool = Executors.newFixedThreadPool( readers);
+    ExecutorService writerPool = Executors.newFixedThreadPool( writers);
+
+    List<IModelObject> nodes = Collections.<IModelObject>singletonList( new ModelObject( "test"));
+
+    Task readerTask = new Task( nodes, false, 10000, 0, new Reader());
+    Task writerTask = new Task( nodes, false, 10000, 0, new Writer());
+    readerPool.execute( readerTask);
+    writerPool.execute( writerTask);
+    
+    writerPool.shutdown();
+    readerPool.shutdown();
+    
+    readerPool.awaitTermination( 1, TimeUnit.DAYS);
+    writerPool.awaitTermination( 1, TimeUnit.DAYS);
+    
+    assertTrue( "Reader did not finish.", readerTask.loops == 0);
+    assertTrue( "Writer did not finish.", writerTask.loops == 0);
+  }
+
+  private static boolean sleep( int time)
+  {
+    try { Thread.sleep( time);} catch( Exception e) { return false;}
+    return true;
+  }
+  
+  private static interface Callback<T>
+  {
+    public void call( T t);
+  }
+  
+  private static class Reader implements Callback<IModelObject>
+  {
+    public void call( IModelObject node)
+    {
+      // read all attributes
+      for( String attrName: node.getAttributeNames())
+        node.getAttribute( attrName);
+      
+      // iterate children
+      for( IModelObject child: node.getChildren())
+      {
+        System.out.println( child.getType());
+      }
+    }
+  }
+  
+  private static class Writer implements Callback<IModelObject>
+  {
+    public void call( IModelObject node)
+    {
+      // write attributes
+      node.setValue( counter++);
+      if ( node.getAttribute( "test") == null)
+        node.setAttribute( "test", "true");
+      else
+        node.removeAttribute( "test");
+      
+      // add/remove children
+      if ( node.getFirstChild( "test") != null)
+        node.getCreateChild( "test");
+      else
+        node.removeChildren( "test");
+    }
+    
+    private int counter;
+  }
+  
+  private static class Task implements Runnable
+  {
+    public Task( List<IModelObject> nodes, boolean shuffle, int loops, int sleep, Callback<IModelObject> callback)
+    {
+      this.nodes = nodes;
+      this.shuffle = shuffle;
+      this.loops = loops;
+      this.sleep = sleep;
+      this.callback = callback;
+    }
+    
+    public void run()
+    {
+      while( loops-- >= 0 && sleep( sleep))
+      {
+        if ( shuffle) Collections.shuffle( nodes);
+        for( IModelObject node: nodes) callback.call( node);
+      }
+    }
+    
+    private List<IModelObject> nodes;
+    private boolean shuffle;
+    private int sleep;
+    private Callback<IModelObject> callback;
+    public int loops;
+  }
+}
