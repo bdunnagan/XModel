f2246db48b4eeecc0f21e116844ef8e4f36677b3
bdunnagan <bdunnagan@192.168.1.103>
2012-10-04 15:04:21 -0400

    ...

--- src/org/xmodel/external/ExternalReference.java
+++ src/org/xmodel/external/ExternalReference.java
@@ -40,16 +40,6 @@ public class ExternalReference extends ModelObject implements IExternalReference
   }
   
   /* (non-Javadoc)
-   * @see java.lang.Object#finalize()
-   */
-  @Override
-  protected void finalize() throws Throwable
-  {
-    System.out.printf( "Finalize: %s\n", getType());
-    super.finalize();
-  }
-
-  /* (non-Javadoc)
    * @see org.xmodel.external.IExternalReference#setCachingPolicy(org.xmodel.external.ICachingPolicy)
    */
   public void setCachingPolicy( ICachingPolicy newCachingPolicy)
