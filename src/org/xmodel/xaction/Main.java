b5e413b699fb4057ed14fc72c1315ce70cb814e1
bdunnagan <bdunnagan@nc.rr.com>
2012-11-20 07:47:55 -0500

    Added support for command-line script execution to Main.

--- src/org/xmodel/xaction/Main.java
+++ src/org/xmodel/xaction/Main.java
@@ -1,19 +1,42 @@
 package org.xmodel.xaction;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-
+import java.io.InputStreamReader;
 import org.xmodel.IModelObject;
 import org.xmodel.xml.XmlIO;
 
 public class Main
 {
-  public static void main( String[] args) throws Exception
+  /**
+   * Run the script in the specified file.
+   * @param file The file.
+   */
+  public static void run( String file) throws Exception
   {
-    XmlIO xmlIO = new XmlIO();
-    IModelObject root = xmlIO.read( new FileInputStream( new File( args[ 0])));
+    IModelObject root = new XmlIO().read( new FileInputStream( new File( file)));
     XActionDocument doc = new XActionDocument( root);
     IXAction script = doc.createScript( root);
     script.run();
+  }
+  
+  public static void main( String[] args) throws Exception
+  {
+    if ( args.length == 0)
+    {
+      String dir = new File( System.getProperty( "user.dir")).getAbsolutePath();
+      BufferedReader reader = new BufferedReader( new InputStreamReader( System.in));
+      while( true)
+      {
+        System.out.printf( "%s> ", dir);
+        String command = reader.readLine().trim();
+        run( command);
+      }
+    }
+    else
+    {
+      run( args[ 0]);
+    }
   }
 }
