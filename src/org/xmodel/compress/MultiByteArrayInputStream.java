c6a3d9411cc8c238fdc69dc886bea8e19ac32bd8
bdunnagan <bdunnagan@nc.rr.com>
2012-12-07 18:42:53 -0500

    ...

---
+++ src/org/xmodel/compress/MultiByteArrayInputStream.java
@@ -0,0 +1,119 @@
+package org.xmodel.compress;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MultiByteArrayInputStream extends InputStream
+{
+  public MultiByteArrayInputStream( List<byte[]> buffers)
+  {
+    segments = buffers;
+  }
+  
+  /* (non-Javadoc)
+   * @see java.io.InputStream#read(byte[], int, int)
+   */
+  @Override
+  public int read( byte[] b, int off, int len) throws IOException
+  {
+    if ( segmentIndex == segments.size()) return -1;
+    
+    byte[] buffer = segments.get( segmentIndex);
+    int space = buffer.length - bufferIndex;
+    if ( space == 0)
+    {
+      if ( ++segmentIndex == segments.size()) return -1;
+      buffer = segments.get( segmentIndex);
+      bufferIndex = 0;
+      space = buffer.length;
+    }
+    
+    int start = off;
+    while( len > space)
+    {
+      System.arraycopy( buffer, bufferIndex, b, off, space);
+      off += space;
+      len -= space;
+      
+      if ( ++segmentIndex == segments.size()) return off - start;
+      
+      buffer = segments.get( segmentIndex);
+      bufferIndex = 0;
+      space = buffer.length;
+    }
+    
+    if ( len > 0) 
+    {
+      System.arraycopy( buffer, bufferIndex, b, off, len);
+      off += len;
+      bufferIndex += len;
+    }
+    
+    return off - start;
+  }
+
+  /* (non-Javadoc)
+   * @see java.io.InputStream#read(byte[])
+   */
+  @Override
+  public int read( byte[] b) throws IOException
+  {
+    return read( b, 0, b.length);
+  }
+
+  /* (non-Javadoc)
+   * @see java.io.InputStream#read()
+   */
+  @Override
+  public int read() throws IOException
+  {
+    if ( segmentIndex == segments.size()) return -1;
+    
+    byte[] buffer = segments.get( segmentIndex);
+    int space = buffer.length - bufferIndex;
+    if ( space == 0)
+    {
+      if ( ++segmentIndex == segments.size()) return -1;
+      buffer = segments.get( segmentIndex);
+      bufferIndex = 0;
+    }
+    
+    return buffer[ bufferIndex++];
+  }
+
+  protected List<byte[]> segments;
+  protected int segmentIndex;
+  protected int bufferIndex;
+  
+  public static void main( String[] args) throws Exception
+  {
+    byte[] bytes = new byte[ 373 * 10000];
+    for( int i=0; i<bytes.length; i++)
+      bytes[ i] = (byte)(i % 128);
+    
+    MultiByteArrayOutputStream out = new MultiByteArrayOutputStream();
+    for( int i=0; i<bytes.length; i+=373)
+      out.write( bytes, i, 373);
+    
+    List<byte[]> buffers = out.getBuffers();
+    System.out.printf( "Count: %d, Max: %d\n", buffers.size(), buffers.get( buffers.size() - 1).length);
+    
+    bytes = new byte[ 10];
+    MultiByteArrayInputStream in = new MultiByteArrayInputStream( buffers);
+    
+    int n = 0;
+    while( true)
+    {
+      int read = in.read( bytes);
+      if ( read == -1) break;
+      
+      for( int i=0; i<read; i++)
+        if ( bytes[ i] != (byte)(n++ % 128))
+          System.err.printf( "Fail: %d\n", i);
+    }
+    
+    if ( in.read() != -1)
+      System.err.printf( "Fail: expected eof\n");
+  }
+}
---
+++ src/org/xmodel/compress/MultiByteArrayOutputStream.java
@@ -0,0 +1,126 @@
+package org.xmodel.compress;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MultiByteArrayOutputStream extends OutputStream
+{
+  public MultiByteArrayOutputStream()
+  {
+    this( 32);
+  }
+  
+  public MultiByteArrayOutputStream( int initialCapacity)
+  {
+    segments = new ArrayList<byte[]>();
+    defaultLength = initialCapacity;
+  }
+  
+  /* (non-Javadoc)
+   * @see java.io.OutputStream#write(byte[], int, int)
+   */
+  @Override
+  public void write( byte[] b, int off, int len) throws IOException
+  {
+    if ( buffer == null) newSegment( defaultLength);
+    
+    int space = buffer.length - bufferIndex;
+    if ( len < space)
+    {
+      System.arraycopy( b, off, buffer, bufferIndex, len);
+      bufferIndex += len;
+    }
+    else
+    {
+      System.arraycopy( b, off, buffer, bufferIndex, space);
+      int remaining = len - space;
+      newSegment( remaining);
+      System.arraycopy( b, off + space, buffer, bufferIndex, remaining);
+      bufferIndex += remaining;
+    }
+  }
+
+  /* (non-Javadoc)
+   * @see java.io.OutputStream#write(byte[])
+   */
+  @Override
+  public void write( byte[] b) throws IOException
+  {
+    write( b, 0, b.length);
+  }
+
+  /* (non-Javadoc)
+   * @see java.io.OutputStream#write(int)
+   */
+  @Override
+  public void write( int b) throws IOException
+  {
+    if ( buffer == null) newSegment( defaultLength);
+    buffer[ bufferIndex++] = (byte)b;
+    if ( bufferIndex == buffer.length) newSegment( defaultLength);
+  }
+
+  /**
+   * Create new segment.
+   * @param length The minimum length.
+   */
+  protected void newSegment( int length)
+  {
+    if ( buffer != null) segments.add( buffer);
+    
+    if ( length < defaultLength) length = defaultLength;
+    buffer = new byte[ length];
+    bufferIndex = 0;
+    
+    if ( defaultLength < maxDefaultLength)
+      defaultLength *= 2;
+  }
+  
+  /**
+   * @return Returns the byte arrays containing the output stream data.
+   */
+  public List<byte[]> getBuffers()
+  {
+    if ( buffer == null) return Collections.emptyList();
+    
+    // compact current segment
+    if ( bufferIndex < defaultLength)
+    {
+      byte[] array = new byte[ bufferIndex];
+      System.arraycopy( buffer, 0, array, 0, bufferIndex);
+      buffer = array;
+    }
+    
+    // add to list
+    segments.add( buffer);
+    
+    // cleanup
+    buffer = null;
+    bufferIndex = 0;
+    
+    return segments;
+  }
+  
+  /**
+   * Returns the total number of bytes in the specified buffers.
+   * @param buffers The buffers.
+   * @return Returns the total number of bytes in the specified buffers.
+   */
+  public static int getLength( List<byte[]> buffers)
+  {
+    int count = 0;
+    for( byte[] buffer: buffers)
+      count += buffer.length;
+    return count;
+  }
+  
+  protected static int maxDefaultLength = 65536;
+  
+  protected int defaultLength;
+  protected List<byte[]> segments;
+  protected byte[] buffer;
+  protected int bufferIndex;
+}
