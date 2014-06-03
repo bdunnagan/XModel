3eac973368303aae32775926f145405ca120600c
bdunnagan <bdunnagan@nc.rr.com>
2014-06-03 10:02:03 -0400

    ...

--- src/org/xmodel/net/nu/test/TestTransport.java
+++ src/org/xmodel/net/nu/test/TestTransport.java
@@ -1,10 +1,12 @@
 package org.xmodel.net.nu.test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
-
 import org.xmodel.IModelObject;
 import org.xmodel.future.AsyncFuture;
+import org.xmodel.future.SuccessAsyncFuture;
 import org.xmodel.net.nu.AbstractTransport;
 import org.xmodel.net.nu.IProtocol;
 import org.xmodel.net.nu.ITransport;
@@ -15,6 +17,7 @@ public class TestTransport extends AbstractTransport
   public TestTransport( IProtocol protocol, IContext transportContext)
   {
     super( protocol, transportContext, Executors.newScheduledThreadPool( 1));
+    transports.add( this);
   }
 
   @Override
@@ -32,6 +35,13 @@ public class TestTransport extends AbstractTransport
   @Override
   public AsyncFuture<ITransport> send( IModelObject message) throws IOException
   {
-    return null;
+    for( TestTransport transport: transports)
+    {
+      if ( transport == this) continue;
+      
+    }
+    return new SuccessAsyncFuture<ITransport>( this);
   }
+  
+  private static List<TestTransport> transports = new ArrayList<TestTransport>();
 }
