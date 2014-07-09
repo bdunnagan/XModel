6de17127255dec3485ba246b723e1d4e523396b9
bdunnagan <bdunnagan@nc.rr.com>
2014-07-09 19:11:35 -0400

    Throw IOException when AMQP connection is used after being closed.

--- src/org/xmodel/net/transport/amqp/AutoRefreshConnection.java
+++ src/org/xmodel/net/transport/amqp/AutoRefreshConnection.java
@@ -264,7 +264,10 @@ public class AutoRefreshConnection
     try
     {
       connectionLock.readLock().lock();
-    
+
+      if ( activeConnection == null || !activeConnection.isOpen())
+        throw new IOException( "Connection is closed.");
+      
       ThreadItem item = threadItems.get();
       if ( item == null || item.channel == null)
       {
