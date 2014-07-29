8779703d22d6d0b1c5e3e3fd04fb30a2d6c06c07
Bob Dunnagan <bdunnagan@nephos6.com>
2014-07-29 14:39:34 -0400

    ...

---
+++ src/org/xmodel/net/nu/amqp/AbstractAmqpTransport.java
@@ -0,0 +1,41 @@
+package org.xmodel.net.nu.amqp;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.xmodel.IModelObject;
+import org.xmodel.future.AsyncFuture;
+import org.xmodel.net.nu.AbstractTransport;
+import org.xmodel.net.nu.ITransport;
+import org.xmodel.net.nu.protocol.Protocol;
+import org.xmodel.xpath.expression.IContext;
+
+public class AbstractAmqpTransport extends AbstractTransport
+{
+  public AbstractAmqpTransport( Protocol protocol, IContext transportContext, ScheduledExecutorService scheduler)
+  {
+    super( protocol, transportContext, scheduler);
+  }
+  
+  @Override
+  public AsyncFuture<ITransport> sendImpl( IModelObject envelope)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public AsyncFuture<ITransport> connect( int timeout)
+  {
+    
+    
+    return null;
+  }
+
+  @Override
+  public AsyncFuture<ITransport> disconnect()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
